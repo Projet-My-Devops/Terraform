87aea14f92064ba68542000ba6569d55de2f15b5
PoissonPluie <corentin.beuf@ynov.com>
2022-10-10 19:59:57 +0200

    Change configuration

---
+++ gitlab/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    proxmox = {
+        source = "Telmate/proxmox"
+        version = "2.9.6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url = "https://192.168.1.2:8006/api2/json"
+  pm_user = "terraform-prov@pve"
+  pm_password = "terraform"
+}
+
+resource "proxmox_vm_qemu" "resource-name" {
+  name = "GITLAB"
+  target_node = "factory"
+  clone = "TEMPLATE"
+  cores = 2
+  memory = 4096
+  oncreate = true
+  onboot = true
+  pool = "Projet-Ydays"
+  agent = 1
+  ipconfig0 = "gw=192.168.10.254,ip=192.168.10.2/24"
+  nameserver = "192.168.10.253"
+  bootdisk = "scsi0"
+  full_clone = true
+  network {
+    bridge = "vmbr2"
+    tag = 10
+    firewall = false
+    link_down = false
+    model = "virtio"
+  }
+  disk {
+    type = "scsi"
+    storage = "DATA"
+    size = "33G"
+  }
+}
+
+output "instance_ips" {
+  value = proxmox_vm_qemu.resource-name.default_ipv4_address
+}
